31e3ecfc7d417f522f3a2a15319a0e769ef9a68c
travisv <vandusseldorptravis@gmail.com>
2015-04-04 00:14:48 -0500

    Create Client validations with model tests

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,5 @@
 class Client < ActiveRecord::Base
+  validates :name, presence: true
+  validates :titleXIX, presence: true, format: { with: /\d\d\d\d\d\d\d[A-Z]/  },
+            uniqueness: true
 end
--- test/models/client_test.rb
+++ test/models/client_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class ClientTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup { @client = Client.create!(name: 'Example User', titleXIX: '1234567A') }
+
+  test 'client should be valid with all attributes' do
+    assert @client.valid?
+  end
+
+  test 'client should be invalid without name' do
+    @client.name = ''
+    assert !@client.valid?
+  end
+
+  test 'client should be invaild without titleXIX number' do
+    @client.titleXIX = ''
+    assert !@client.valid?
+  end
+
+  test 'client should be invalid with improper titleXIX format' do
+    invalid_titleXIX = %w[ 123456A 123ADFS 1234567a A1234567 ]
+    invalid_titleXIX.each do |invalid|
+      @client.titleXIX = invalid
+    end
+    assert !@client.valid?
+  end
+
+  test 'client should be valid with proper titleXIX format' do
+    valid_titleXIX = %w[ 1234567A 3420952Z 9823111G 0000000S ]
+    valid_titleXIX.each do |valid|
+      @client.titleXIX = valid
+    end
+    assert @client.valid?
+  end
+
+  test 'clients titleXIX number should be unique' do
+    duplicate_client = @client.dup
+    assert !duplicate_client.valid?
+  end
 end
