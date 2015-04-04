390ae713dff62343c4bae5b873780dd6bcd7d0e7
travisv <vandusseldorptravis@gmail.com>
2015-04-03 23:39:06 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hv::Application.config.secret_key_base = '295bdcf0a55d71a9706b0d75d7610d531c30151b204d4905d87f953b0b8b61372846a179805a084cec72fa1638ef370e34c40fce3d32166bced8d3e012fafb19'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hv::Application.config.session_store :cookie_store, key: '_hv_session'
